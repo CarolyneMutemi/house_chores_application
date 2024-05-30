87f1361f352f4046cbc78ba7dcba9643d6efa1de
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2024-05-30 11:50:02 +0300

    Created the users endpoint views.

---
+++ Backend/api/v1/utils/usersUtil.py
@@ -0,0 +1,185 @@
+"""
+Users utilities.
+"""
+
+import os
+from dotenv import load_dotenv
+from pymongo import ReturnDocument
+from typing import Dict, List
+from uuid import uuid4
+import requests
+import bcrypt
+from bson import ObjectId
+from pymongo import ReturnDocument
+from api.v1.views import mongo, redis_client
+
+load_dotenv()
+
+
+def _hash_password(password: str) -> bytes:
+    """
+    Takes in a password string and returns a salted harsh of it.
+    """
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """
+    Returns a string representation of a new UUID.
+    """
+    return str(uuid4())
+
+class Users:
+    """
+    Handles users data.
+    """
+    
+    @staticmethod
+    def register_user(first_name: str, last_name: str, email: str, password: str) -> Dict:
+        """
+        Registers user.
+        """
+        hashed_password = _hash_password(password).decode('utf-8')
+        session_id = _generate_uuid()
+        user_exists = Users.get_user_by_email(email)
+        if user_exists:
+            return {'error': 'user exists'}
+        api_key = os.getenv('API_KEY')
+        validate_email = requests.post("https://api.verifalia.com/v2.5/email-validations",
+                                       headers={"Content-Type": "application/json"},
+                                       auth=('carolmutemi', api_key),
+                                       data="{entries: [{inputData: '%s'}]}" %email,
+                                       timeout=10)
+        is_deliverable = validate_email.json()['entries']['data'][0]['classification']
+        if is_deliverable == 'Undeliverable':
+            return {'error': 'mail undeliverable'}
+        users_collection = mongo.db.users
+        user = users_collection.insert_one({'first_name': first_name, 'last_name': last_name, 'email': email, 'password': hashed_password, 'active_chores': []})
+        expiration_time = 86400
+        user_id = str(user.inserted_id)
+        redis_client.setex(f'user_{session_id}', expiration_time, user_id)
+        return {"session_id": session_id, "expiration_time": expiration_time}
+
+
+    @staticmethod
+    def get_user_by_email(email: str) -> Dict:
+        """
+        Checks if a user already exists.
+        """
+        users_collection = mongo.db.users
+        user = users_collection.find_one({'email': email})
+        if user:
+            user['id'] = str(user['_id'])
+            del user['_id']
+        return user
+    
+
+    @staticmethod
+    def log_in(email: str, password: str) -> Dict:
+        """
+        Logs in a user.
+        """
+        user = Users.get_user_by_email(email)
+        if not user:
+            return None
+
+        is_password = bcrypt.checkpw(password.encode(), user['password'].encode())
+        if not is_password:
+            return {'error': 'password incorrect'}
+
+        session_id = _generate_uuid()
+        expiration_time = 86400 # 5 minutes
+        user_id = user['id']
+        redis_client.setex(f'user_{session_id}', expiration_time, user_id )
+        return {"session_id": session_id, "expiration_time": expiration_time}
+
+
+    @staticmethod
+    def log_out(session_id: str) -> Dict:
+        """
+        Logs out a user.
+        """
+        key = f"user_{session_id}"
+        if redis_client.exists(key):
+            redis_client.delete(key)
+            return {"success": "user session deleted"}
+        return None
+    
+    @staticmethod
+    def get_user_id_by_session_id(session_id: str) -> str:
+        """
+        Gets a user by session_id.
+        """
+        key = f"user_{session_id}"
+        if redis_client.exists(key):
+            user_id = redis_client.get(key)
+            return user_id.decode('utf-8')
+        return None
+    
+    @staticmethod
+    def get_user_from_session_id(session_id: str) -> Dict:
+        """
+        Gets user.
+        """
+        user_id = Users.get_user_id_by_session_id(session_id)
+        if not user_id:
+            return None
+        users_collection = mongo.db.users
+        try:
+            user = users_collection.find_one({'_id': ObjectId(user_id)}, {'password': 0})
+            if user:
+                user['id'] = str(user['_id'])
+                del user['_id']
+        except:
+            return None
+        else:
+            return user
+
+
+    @staticmethod
+    def get_user_from_id(user_id: str) -> Dict:
+        """
+        Gets user.
+        """
+        users_collection = mongo.db.users
+        user = users_collection.find_one({'_id': ObjectId(user_id)})
+        return user
+
+
+    @staticmethod
+    def get_my_current_chores(session_id: str) -> List:
+        """
+        Tracks my chores.
+        """
+        from api.v1.utils.choresUtilities import Chores
+        user = Users.get_user_from_session_id(session_id)
+        if not user:
+            return None
+        user_chores = user.get('active_chores')
+        for chore in user_chores:
+            chore_token = chore.get('chore_token')
+            if redis_client.exists(f'chore_{chore_token}'):
+                continue
+            else:
+                Chores.delete_expired_chore(chore)
+        updated_user = Users.get_user_from_session_id(session_id)
+        return updated_user.get('active_chores')
+
+    @staticmethod
+    def post_review(session_id, provider_id, review) -> Dict:
+        """
+        Post a review.
+        """
+        from api.v1.utils.providersUtils import Providers
+        user = Users.get_user_from_session_id(session_id)
+        if not user:
+            return None
+        providers_collection = mongo.db.providers
+        user_first_name = user.get('first_name')
+        review_object = {'client_name': user_first_name, 'review': review}
+        provider = providers_collection.find_one_and_update({'_id': ObjectId(provider_id)},
+                                                            {'$push': {'reviews': review_object}},
+                                                            return_document=ReturnDocument.AFTER)
+        provider['id'] = str(provider['_id'])
+        del provider['_id']
+        return provider
---
+++ Backend/api/v1/views/users.py
@@ -0,0 +1,104 @@
+"""
+Users views.
+"""
+
+from flask import jsonify, request, abort
+from api.v1.views import app_views
+from api.v1.utils.usersUtil import Users
+
+
+@app_views.route('/user/<session_id>', strict_slashes=False)
+def get_user_by_session(session_id):
+    """
+    Get user by session_id.
+    """
+    user = Users.get_user_from_session_id(session_id)
+    if not user:
+        return jsonify({'error': 'user not found'}), 403
+    return jsonify(user)
+
+
+@app_views.route('/register', strict_slashes=False, methods=['POST'])
+def register_client():
+    """
+    Registers a client.
+    """
+    first_name = request.form.get('first_name')
+    if not first_name:
+        return jsonify({'error': 'first_name missing'}), 400
+    last_name = request.form.get('last_name')
+    if not last_name:
+        return jsonify({'error': 'last_name missing'}), 400
+    email = request.form.get('email')
+    if not email:
+        return jsonify({'error': 'email missing'}), 400
+    password = request.form.get('password')
+    if not password:
+        return jsonify({'error': 'password missing'}), 400
+    result = Users.register_user(first_name, last_name, email, password)
+    error = result.get('error', None)
+    if error == 'user exists':
+        return jsonify(result), 409
+    if error == 'mail undeliverable':
+        return jsonify(result), 403
+    return jsonify(result), 200
+
+
+@app_views.route('/login', methods=['POST'], strict_slashes=False)
+def user_login():
+    """
+    Log in endpoint.
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not email:
+        return jsonify({'error': 'missing email'}), 403
+    if not password:
+        return jsonify({'error': 'missing password'}), 403
+
+    result = Users.log_in(email, password)
+    if not result:
+        return jsonify({'error': 'email not found'}), 403
+    if 'error' in result:
+        return jsonify(result), 403
+    return jsonify(result)
+
+
+@app_views.route('/logout/<session_id>', strict_slashes=False)
+def logout(session_id):
+    """
+    Logs out a user.
+    """
+    result = Users.log_out(session_id)
+
+    if not result:
+        return jsonify({'error': 'user not found'}), 403
+    return jsonify(result)
+
+
+
+@app_views.route('/my_chores/<session_id>')
+def get_my_chores(session_id):
+    """
+    Get my chores.
+    """
+    result = Users.get_my_current_chores(session_id)
+    if result is None:
+        return jsonify({'error': 'user not found'}), 403
+    return jsonify(result)
+
+
+@app_views.route('/post_review/<session_id>/<provider_id>', methods=['POST'], strict_slashes=False)
+def post_review(session_id, provider_id):
+    """
+    Posts a review.
+    """
+    review = request.form.get('review')
+    if not review:
+        return jsonify({'error': 'review missing'}), 403
+    result = Users.post_review(session_id, provider_id, review)
+
+    if not result:
+        return jsonify({'error': 'user or provider not found'}), 403
+    return jsonify(result)
