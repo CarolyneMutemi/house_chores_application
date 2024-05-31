a2ab24da36d7f41d9984a620c8ab4f7a5d2ebe8f
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2024-05-31 10:22:50 +0300

    Added the rates component.

---
+++ house-chores/src/rates.jsx
@@ -0,0 +1,48 @@
+
+export default function Rates() {
+    return (
+        <>
+        <div className="align-rates">
+            <div className="rates-pop-up">
+              <span className="icon-close"><img src="close-circle.svg" alt="close icon" /></span>
+              <img src="banana-rates.png" alt="Minion hugging bananas." className="minion-rates"/>
+              <h3>Sharon Muthoni's Rates</h3>
+              <div className="scroll-rates">
+                <div className="rate-category">
+                  <h4>One bedroom</h4>
+                  <p>8k - 10k </p>
+                </div>
+                <div className="rate-category">
+                  <h4>One bedroom</h4>
+                  <p>8k - 10k </p>
+                </div>
+                <div className="rate-category">
+                  <h4>One bedroom</h4>
+                  <p>8k - 10k </p>
+                </div>
+                <div className="rate-category">
+                  <h4>One bedroom</h4>
+                  <p>8k - 10k </p>
+                </div>
+                <div className="rate-category">
+                  <h4>One bedroom</h4>
+                  <p>8k - 10k </p>
+                </div>
+                <div className="rate-category">
+                  <h4>One bedroom</h4>
+                  <p>8k - 10k </p>
+                </div>
+                <div className="rate-category">
+                  <h4>One bedroom</h4>
+                  <p>8k - 10k </p>
+                </div>
+                <div className="rate-category">
+                  <h4>One bedroom</h4>
+                  <p>8k - 10k </p>
+                </div>
+              </div>
+            </div>
+        </div>
+        </>
+    )
+}
